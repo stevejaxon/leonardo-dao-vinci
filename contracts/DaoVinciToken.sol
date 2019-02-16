a9da21103614aec5568124e95ffeec56e831d71f
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2019-02-16 11:45:13 -0700

    feat: removing the strings library and fixing issues with inheritance

--- contracts/DaoVinciToken.sol
+++ contracts/DaoVinciToken.sol
@@ -1,31 +1,18 @@
 pragma solidity ^0.5.0;
 
-import "openzeppelin-eth/contracts/ownership/Ownable.sol";
-import "openzeppelin-eth/contracts/token/ERC721/ERC721Mintable.sol";
+import "openzeppelin-eth/contracts/token/ERC721/ERC721MetadataMintable.sol";
 import "./ProxyRegistry.sol";
-import './Strings.sol';
 
-contract DaoVinciToken is ERC721Mintable, Ownable {
+contract DaoVinciToken is ERC721MetadataMintable {
     // Used
     address proxyRegistryAddress;
 
     // Zepkit has a pattern of initializing instead of constructing instance
-    function initialize(address _proxyRegistryAddress) initializer public {
+    function initialize(string memory name, string memory symbol, address _proxyRegistryAddress) initializer public {
         proxyRegistryAddress = _proxyRegistryAddress;
-    }
-
-  /**
-   * @dev Returns an URI for a given token ID
-   */
-    function tokenURI(uint256 _tokenId) public view returns (string memory) {
-        return Strings.strConcat(
-            baseTokenURI(),
-            Strings.uint2str(_tokenId)
-        );
-    }
-
-    function baseTokenURI() public view returns (string memory) {
-        return "https://opensea-creatures-api.herokuapp.com/api/creature/";
+        ERC721.initialize();
+        ERC721Metadata.initialize(name, symbol);
+        ERC721MetadataMintable.initialize(msg.sender);
     }
 
   /**
