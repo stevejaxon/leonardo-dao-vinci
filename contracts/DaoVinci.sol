1f3717506a96e1e0e74c9a5ede8b2e8d04b60a05
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2019-02-17 04:32:28 -0700

    fix: removing the experimental solidity features to resolve an issue with deploying a sub-graph

--- contracts/DaoVinci.sol
+++ contracts/DaoVinci.sol
@@ -1,4 +1,4 @@
-pragma experimental ABIEncoderV2;
+pragma solidity ^0.5.0;
 
 import "zos-lib/contracts/Initializable.sol";
 import "openzeppelin-eth/contracts/ownership/Ownable.sol";
@@ -8,26 +8,23 @@ contract DaoVinci is Initializable, Ownable {
     event RewardDistributed(address payee, uint amount);
     event BalanceWithdrawn(address payee, uint amount);
 
-    struct Reward {
-        address payee;
-        uint amount;
-    }
-
     mapping(address => uint) public balances;
 
     function initialize() initializer public {
         Ownable.initialize(msg.sender);
     }
 
-    function distributeRewards(Reward[] memory _rewards) public payable onlyOwner {
+    function distributeRewards(address[] memory _payees, uint[] memory _amounts) public payable onlyOwner {
+        require(_payees.length == _amounts.length);
         uint soldPrice = msg.value;
         uint totalRewards;
-        for (uint i = 0; i < _rewards.length; i++) {
-            Reward memory reward = _rewards[i];
-            bool balanceIncreased = _increaseBalance(reward);
+        for (uint i = 0; i < _payees.length; i++) {
+            address _payee = _payees[i];
+            uint _amount = _amounts[i];
+            bool balanceIncreased = _increaseBalance(_payee, _amount);
             if (balanceIncreased) {
-                totalRewards = totalRewards + reward.amount;
-                emit RewardDistributed(reward.payee, reward.amount);
+                totalRewards = totalRewards + _amount;
+                emit RewardDistributed(_payee, _amount);
             }
         }
         require(soldPrice >= totalRewards);
@@ -46,9 +43,9 @@ contract DaoVinci is Initializable, Ownable {
         emit BalanceWithdrawn(usersAddress, balance);
     }
 
-    function _increaseBalance(Reward memory _reward) private returns(bool) {
-        if (_reward.amount > 0) {
-            balances[_reward.payee] = balances[_reward.payee] + _reward.amount;
+    function _increaseBalance(address payee, uint amount) private returns(bool) {
+        if (amount > 0) {
+            balances[payee] = balances[payee] + amount;
             return true;
         }
         return false;
