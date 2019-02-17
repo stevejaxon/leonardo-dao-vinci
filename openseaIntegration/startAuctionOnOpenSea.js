4a953a497f3db321e06e4dcf9fcd1bacf9872306
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2019-02-16 19:42:11 -0700

    feat: before rewrite based on OpenSeaCreatures

--- openseaIntegration/startAuctionOnOpenSea.js
+++ openseaIntegration/startAuctionOnOpenSea.js
@@ -1,19 +1,39 @@
-const Web3 =  require('web3');
-const Opensea = require('opensea-js');
+require('dotenv').config();
+const opensea = require('opensea-js');
+const OpenSeaPort = opensea.OpenSeaPort;
+const Network = opensea.Network;
 
-const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io');
+const MnemonicWalletSubprovider = require('@0x/subproviders').MnemonicWalletSubprovider;
+const RPCSubprovider = require('web3-provider-engine/subproviders/rpc');
+const Web3ProviderEngine = require('web3-provider-engine');
+const MNEMONIC = process.env.MNENOMIC;
+const INFURA_KEY = 'Iw4pUdWfzz9ZxddqpXrS';
+const NETWORK = Network.Rinkeby;
 
-const seaport = new Opensea.OpenSeaPort(provider, {
-    networkName: Opensea.Network.Main
+const BASE_DERIVATION_PATH = `44'/60'/0'/0`;
+const mnemonicWalletSubprovider = new MnemonicWalletSubprovider({ mnemonic: MNEMONIC, baseDerivationPath: BASE_DERIVATION_PATH});
+const infuraRpcSubprovider = new RPCSubprovider({
+    rpcUrl: 'https://rinkeby.infura.io/' + INFURA_KEY,
 });
 
-const tokenAddress = '0x100A1698c3fBb4a1F3B2ED74B5e39741aD233e89';
+const providerEngine = new Web3ProviderEngine()
+providerEngine.addProvider(mnemonicWalletSubprovider)
+providerEngine.addProvider(infuraRpcSubprovider)
+providerEngine.start();
+
+const seaport = new OpenSeaPort(providerEngine, {
+    networkName: Network.Rinkeby
+}, (arg) => console.log(arg));
+
+const tokenAddress = '0x100a1698c3fbb4a1f3b2ed74b5e39741ad233e89';
+const accountAddress = '0xf0f2077bc2361af6ae805609111c2b7f1594a74b';
 const startAmount = 100;
 const endAmount = 0;
 
 class OpenSeaAuction {
 
     async auction(tokenId) {
+        console.log('about to call createSellOrder')
         // Expire this auction one day from now
         const expirationTime = (Date.now() / 1000 + 60 * 60 * 24);
         // If `endAmount` is specified, the order will decline in value to that amount until `expirationTime`. Otherwise, it's a fixed-price order.
@@ -21,7 +41,7 @@ class OpenSeaAuction {
     }
 }
 
-module.exports = OpenSeaAuction
+module.exports = OpenSeaAuction;
 
 
 
