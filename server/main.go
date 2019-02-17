ca38690b228d88be516f149d227645bace751d84
Piers Powlesland <piers.powlesland@yoti.com>
2019-02-17 04:19:02 +0000

    Add debug log output for vote endpoint

--- server/main.go
+++ server/main.go
@@ -44,20 +44,6 @@ func main() {
 		}
 	}
 
-	/*
-		v := &voteMessage{
-			UserAddress: "0x931d387731bbbc988b312206c74f77d004d6b84b",
-			Period:      2,
-			Images:      []int{2, 5, 6, 7, 8},
-		}
-		data, err := json.Marshal(v)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-
-		fmt.Printf("%s\n", string(data))
-	*/
 	imageHandler := http.StripPrefix("/images/", http.FileServer(http.Dir("images")))
 	imageHandlerFunc := func(w http.ResponseWriter, r *http.Request) {
 		imageHandler.ServeHTTP(w, r)
@@ -98,10 +84,14 @@ func handleIteration(w http.ResponseWriter, r *http.Request) {
 
 func handleVotes(w http.ResponseWriter, r *http.Request) {
 	content, err := ioutil.ReadAll(r.Body)
+	fmt.Printf("content: %s\n", string(content))
 	v := &voteMessage{}
 	err = json.Unmarshal(content, v)
 	if err != nil {
-		http.Error(w, "Could not unmarshal votes message", http.StatusBadRequest)
+		errmsg := "Could not unmarshal votes message"
+		fmt.Printf("%s\n", errmsg)
+		http.Error(w, errmsg, http.StatusBadRequest)
+		fmt.Printf("offending content: %s\n", string(content))
 		return
 	}
 
@@ -109,7 +99,9 @@ func handleVotes(w http.ResponseWriter, r *http.Request) {
 	for _, voters := range votes {
 		_, ok := voters[v.UserAddress]
 		if ok {
-			http.Error(w, fmt.Sprintf("User %q has already voted in this iteration", v.UserAddress), http.StatusBadRequest)
+			errmsg := fmt.Sprintf("User %q has already voted in this iteration", v.UserAddress)
+			fmt.Printf("%s\n", errmsg)
+			http.Error(w, errmsg, http.StatusBadRequest)
 			return
 		}
 	}
